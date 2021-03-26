cff7ea0b5fc86be316e8c84e7ec87380fd89bbfc
Dhruva <dhruvas.stacksum@gmail.com>
2021-03-26 18:45:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BarCharts } from './components/BarCharts';
+import { Dough } from './components/Dough';
+import { PieChart } from './components/PieChart';
+import { Polars } from './components/Polar';
+import { Radars } from './components/Radars';
+import { Lines } from './components/Line';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BarCharts/>
+      <Dough/>
+      <PieChart/>
+      <Polars/>
+      <Radars/>
+      <Lines/>
     </div>
   );
 }
